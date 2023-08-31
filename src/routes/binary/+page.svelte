92e3f627ab5fd0d19aa263ef2af4d6a203ad13c6
IzzIzzIzzIzzI <iskoa002@osloskolen.no>
2023-08-31 10:39:43 +0200

    Start of binary and fixed css colors

---
+++ src/routes/binary/+page.svelte
@@ -0,0 +1,27 @@
+<div class="input br">
+    
+</div>
+
+<div class="output br">
+
+</div>
+
+<style>
+    .input {
+        height: 200px;
+        width: 400px;
+        background-color: #3D3C3E;
+        position: absolute;
+        top: 150px;
+        left: 200px;
+    }
+
+    .output {
+        height: 200px;
+        width: 400px;
+        background-color: #3D3C3E;
+        position: absolute;
+        top: 150px;
+        right: 200px;
+    }
+</style>

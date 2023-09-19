b0f9dcd3b2658cf9b83e54cac4eef2e4cbbd5577
IzzIzzIzzIzzI <iskoa002@osloskolen.no>
2023-09-19 14:20:44 +0200

    ye

---
+++ src/lib/components/Img_morse_t.svelte
@@ -0,0 +1,23 @@
+<svg id="morse_t" data-name="test" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 744.53 470.18">
+    <text class="morse_text" transform="translate(0 25.91)"><tspan x="0" y="0" xml:space="preserve">-- --- .-. ... .  -- --- .-. ... .  -- </tspan><tspan x="0" y="36" xml:space="preserve">--- .-. ... .  -- --- .-. </tspan><tspan x="0" y="72" xml:space="preserve">... .  -- --- .-. ... .  -- </tspan><tspan x="0" y="108" xml:space="preserve">--- .-. ... .  -- </tspan><tspan x="0" y="144" xml:space="preserve">--- .-. ... .  -- --- .-. ... </tspan><tspan x="0" y="180" xml:space="preserve">.  -- --- .-. ... .  -- --- .-. ... .  </tspan><tspan x="0" y="216" xml:space="preserve">-- --- .-. ... .  </tspan><tspan x="0" y="252">-- --- </tspan><tspan x="0" y="288" xml:space="preserve">.-. ... .  -- </tspan><tspan x="0" y="324" xml:space="preserve">--- .-. ... .  -- --- </tspan><tspan x="0" y="360" xml:space="preserve">.-. ... .  </tspan><tspan x="0" y="396">-- --- .-. </tspan><tspan x="0" y="432" xml:space="preserve">... .  </tspan></text>
+  </svg>
+  
+<style>
+
+    #morse_t {
+        position: absolute;
+        top: 0px;
+        left: 0px;
+        width: 40%;
+        border: red solid 1px;
+    }
+
+    .morse_text {
+        fill: #00ff62;
+        opacity: .2;
+        font-family: InputMono-Bold, 'Input Mono';
+        font-size: 40px;
+        letter-spacing: 5px;
+        user-select: none;
+    }
+</style>

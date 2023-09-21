fe0d6406d1a5d7802b519e7381e5059e7833eab4
IzzIzzIzzIzzI <iskoa002@osloskolen.no>
2023-09-21 09:20:47 +0200

    Added the background for the a1z26 page

---
+++ src/lib/components/Img_a1z26_b.svelte
@@ -0,0 +1,23 @@
+<svg id="a1z26_b" data-name="a1z26_b" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1353.16 766.28">
+    <text class="a1z26_text" transform="translate(1338.16 669.92)"><tspan x="0" y="0">22</tspan><tspan x="0" y="60"> </tspan><tspan x="0" y="66.36">5</tspan><tspan x="-36" y="-132.72">18</tspan><tspan x="-36" y="-72.72"> </tspan><tspan x="-36" y="-66.36">25</tspan><tspan class="cls-1" x="-36" y="-6.36"> </tspan><tspan x="-36" y="0">5</tspan><tspan x="-36" y="30"> </tspan><tspan x="-36" y="36.36">16</tspan><tspan x="-72" y="-36.36">9</tspan><tspan x="-72" y="-6.36"> </tspan><tspan x="-72" y="0">3</tspan><tspan class="cls-1" x="-72" y="30"> </tspan><tspan x="-72" y="36.36">19</tspan><tspan x="-108" y="-308.16">1</tspan><tspan x="-108" y="-278.16"> </tspan><tspan x="-108" y="-271.8">21</tspan><tspan class="cls-1" x="-108" y="-211.8"> </tspan><tspan x="-108" y="-205.44">3</tspan><tspan x="-108" y="-175.44"> </tspan><tspan x="-108" y="-169.08">5</tspan><tspan x="-108" y="-139.08"> </tspan><tspan x="-108" y="-132.72">22</tspan><tspan class="cls-1" x="-108" y="-72.72"> </tspan><tspan x="-108" y="-66.36">5</tspan><tspan x="-108" y="-36.36"> </tspan><tspan x="-108" y="-30">18</tspan><tspan x="-108" y="30"> </tspan><tspan x="-108" y="36.36">25</tspan><tspan x="-144" y="-175.44">5</tspan><tspan x="-144" y="-145.44"> </tspan><tspan x="-144" y="-139.08">16</tspan><tspan class="cls-1" x="-144" y="-79.08"> </tspan><tspan x="-144" y="-72.72">9</tspan><tspan x="-144" y="-42.72"> </tspan><tspan x="-144" y="-36.36">3</tspan><tspan x="-144" y="-6.36"> </tspan><tspan x="-144" y="0">19</tspan><tspan class="cls-1" x="-144" y="60"> </tspan><tspan x="-144" y="66.36">1</tspan><tspan x="-180" y="0">21</tspan><tspan x="-180" y="60"> </tspan><tspan x="-180" y="66.36">3</tspan><tspan x="-216" y="-102.72">5</tspan><tspan x="-216" y="-72.72"> </tspan><tspan x="-216" y="-66.36">22</tspan><tspan class="cls-1" x="-216" y="-6.36"> </tspan><tspan x="-216" y="0">5</tspan><tspan x="-216" y="30"> </tspan><tspan x="-216" y="36.36">18</tspan><tspan x="-252" y="-241.8">25</tspan><tspan x="-252" y="-181.8"> </tspan><tspan x="-252" y="-175.44">5</tspan><tspan class="cls-1" x="-252" y="-145.44"> </tspan><tspan x="-252" y="-139.08">16</tspan><tspan x="-252" y="-79.08"> </tspan><tspan x="-252" y="-72.72">9</tspan><tspan x="-252" y="-42.72"> </tspan><tspan x="-252" y="-36.36">3</tspan><tspan class="cls-1" x="-252" y="-6.36"> </tspan><tspan x="-252" y="0">19</tspan><tspan x="-252" y="60"> </tspan><tspan x="-252" y="66.36">1</tspan><tspan x="-288" y="-36.36">21</tspan><tspan x="-288" y="23.64"> </tspan><tspan x="-288" y="30">3</tspan><tspan class="cls-1" x="-288" y="60"> </tspan><tspan x="-288" y="66.36">5</tspan><tspan x="-324" y="-169.08">22</tspan><tspan x="-324" y="-109.08"> </tspan><tspan x="-324" y="-102.72">5</tspan><tspan class="cls-1" x="-324" y="-72.72"> </tspan><tspan x="-324" y="-66.36">18</tspan><tspan x="-324" y="-6.36"> </tspan><tspan x="-324" y="0">25</tspan><tspan x="-324" y="60"> </tspan><tspan x="-324" y="66.36">5</tspan><tspan x="-360" y="36.36">16</tspan><tspan x="-396" y="-72.72">9</tspan><tspan x="-396" y="-42.72"> </tspan><tspan x="-396" y="-36.36">3</tspan><tspan class="cls-1" x="-396" y="-6.36"> </tspan><tspan x="-396" y="0">19</tspan><tspan x="-396" y="60"> </tspan><tspan x="-396" y="66.36">1</tspan><tspan x="-432" y="-36.36">21</tspan><tspan x="-432" y="23.64"> </tspan><tspan x="-432" y="30">3</tspan><tspan class="cls-1" x="-432" y="60"> </tspan><tspan x="-432" y="66.36">5</tspan><tspan x="-468" y="36.36">22</tspan><tspan x="-504" y="66.36">5</tspan></text>
+</svg>
+
+<style>
+    #a1z26_b {
+        position: absolute;
+        bottom: 0px;
+        right: 0px;
+        width: 90%;
+    }
+
+    .a1z26_text {
+        fill: #00ff62;
+        opacity: .2;
+        font-family: InputMono-Regular, 'Input Mono';
+        font-size: 30px;
+        glyph-orientation-vertical: 0deg;
+        text-orientation: upright;
+        writing-mode: tb;
+        user-select: none;
+    }
+</style>

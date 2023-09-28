42b2a5e9270279b7d327861cfe116021875d0c81
Jun1l1ll1 <jujoa007@osloskolen.no>
2023-09-28 10:11:38 +0200

    made fixed distance for change btn

--- src/routes/caesar/+page.svelte
+++ src/routes/caesar/+page.svelte
@@ -199,7 +199,7 @@
         height: 100%;
         display: flex;
         flex-direction: column;
-        justify-content: center;
+        padding-top: 120px;
         align-items: center;
     }
 
